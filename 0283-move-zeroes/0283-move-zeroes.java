d1ef85380c5e8399c55dc8d1e25fa36786f73bc4
Sruti Ranjan Pradhan <139843251+SR-Pradhan@users.noreply.github.com>
2026-07-06 12:05:51 +0530

    Time: 2 ms (92.13%), Space: 47.7 MB (56.63%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,28 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        int left = 0;
 
-        int n = nums.length;
-        int j = 0;
-        int count = 0;
-        
-        //1st move non-zeros elements to left side
-        for(int i = 0; i < n; i++){
-            if(nums[i] != 0){
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
-                count++;
+        for (int right = 0; right < nums.length; right++) {
+            if (nums[right] != 0) {
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
+                left++;
             }
-        }
-
-        // then fill rest position with zeros.
-        for(int i = count; i < n; i++){
-            nums[i] = 0;
         }
     }
 }
-//Approach: Optimal App.
-//Pattern: Two Pointer
-//T.C: O(n)
-//S.C: O(1)
