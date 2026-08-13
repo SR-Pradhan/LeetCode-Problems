bc33bd9391ed422939d6c80a393cac5b8031413e
Sruti Ranjan Pradhan <139843251+SR-Pradhan@users.noreply.github.com>
2026-08-13 21:42:13 +0530

    Time: 0 ms (100%), Space: 44.3 MB (39.17%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -8,44 +8,59 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        if(list1 == null && list2 == null) return null;
-        if(list1 == null) return list2;
-        if(list2 == null) return list1;
+        // Handle cases where one or both lists are empty
+        if (list1 == null && list2 == null) {
+            return null;
+        }
 
-        ListNode head = null;
+        if (list1 == null) {
+            return list2;
+        }
 
-        if(list1.val <= list2.val){
+        if (list2 == null) {
+            return list1;
+        }
+
+        ListNode head;
+
+        // Choose the smaller first node as the head
+        if (list1.val <= list2.val) {
             head = list1;
             list1 = list1.next;
-        }else{
+        } else {
             head = list2;
             list2 = list2.next;
         }
 
+        // temp always points to the last node of the merged list
         ListNode temp = head;
 
-        while(list1 != null && list2 != null){
+        // Compare nodes while both lists still have elements
+        while (list1 != null && list2 != null) {
 
-        if(list1.val <= list2.val){
-            temp.next = list1;
-            list1 = list1.next;
-        }else{
-            temp.next = list2;
-            list2  = list2.next;
-        } 
+            if (list1.val <= list2.val) {
+                temp.next = list1;
+                list1 = list1.next;
+            } else {
+                temp.next = list2;
+                list2 = list2.next;
+            }
 
-         temp = temp.next;
+            // Move temp to the newly attached node
+            temp = temp.next;
         }
-        
-        if(list1 == null){
+
+        // Attach the remaining nodes of the non-empty list
+        if (list1 == null) {
             temp.next = list2;
-        }else{
+        } else {
             temp.next = list1;
         }
-        
+
         return head;
     }
 }
