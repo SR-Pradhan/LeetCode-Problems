b8f210c3deadb84c4e3e40d034a731095707d375
Sruti Ranjan Pradhan <139843251+SR-Pradhan@users.noreply.github.com>
2026-09-01 21:56:22 +0530

    Time: 18 ms (12.83%), Space: 73.9 MB (26.3%) - LeetHub

--- 0041-first-missing-positive/0041-first-missing-positive.java
+++ 0041-first-missing-positive/0041-first-missing-positive.java
@@ -1,26 +1,12 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        Arrays.sort(nums);
 
-        int n = nums.length;
-        int max = 0;
+        int missing = 1;
 
-        // Store all elements in a HashSet
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int i = 0; i < n; i++) {
-            set.add(nums[i]);
-            max = Math.max(max, nums[i]);
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] == missing) missing++;
         }
-
-        // Find the first missing positive number
-        for (int i = 1; i <= max; i++) {
-
-            if (!set.contains(i)) {
-                return i;
-            }
-        }
-
-        // If all numbers from 1 to max are present
-        return max + 1;
+        return missing;
     }
 }
