992805b892fc06ef683c931352b81e054ede193c
Sruti Ranjan Pradhan <139843251+SR-Pradhan@users.noreply.github.com>
2026-09-08 10:35:15 +0530

    Time: 6 ms (8.21%), Space: 44.8 MB (86.52%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,36 +1,26 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-        int n = nums1.length;
-        int m = nums2.length;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
 
-        int[] ans = new int[n];
+        int[] ans = new int[n1];
 
-        for (int i = 0; i < n; i++) {
-            int max = -1;
-
-            // Find nums1[i] inside nums2
-            for (int j = 0; j < m; j++) {
-
-                if (nums2[j] == nums1[i]) {
-
-                    // Search for the first greater element to the right
-                    for (int k = j + 1; k < m; k++) {
-
-                        if (nums2[k] > nums1[i]) {
-                            max = nums2[k];
+        for(int i = 0; i < n1; i++){
+            for(int j = 0; j < n2; j++){
+                if(nums1[i] == nums2[j]){
+                    for(int k = j; j < n2; j++){
+                        if(nums1[i] < nums2[j]){
+                            ans[i] = nums2[j];
                             break;
+                        }else{
+                            ans[i] = -1;
                         }
                     }
-
-                    // nums1[i] found, no need to search further
-                    break;
                 }
             }
-
-            ans[i] = max;
+            
         }
-
         return ans;
     }
 }
